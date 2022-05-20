8a0f33a18f88e0b218ed96311d4af18fb22b6e84
jugen <jugendrabhati658@gmail.com>
2022-05-20 18:11:12 +0530

    20-05-2022 || 18:00 PM

--- lib/form/old_visit_form.dart
+++ lib/form/old_visit_form.dart
@@ -1,115 +1,185 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:stemro_app/view/home_screen.dart';
-import '../auth/AuthService.dart';
-import 'dart:async';
 
 class OldVisits extends StatefulWidget {
+  const OldVisits({Key? key}) : super(key: key);
+
   @override
-  State<OldVisits> createState() => _FormPageState();
+  State<OldVisits> createState() => _OldVisits();
 }
-class _FormPageState extends State<OldVisits> {
 
-  List data = [];
+class _OldVisits extends State<OldVisits> {
+  late Query _ref;
+  DatabaseReference reference = FirebaseDatabase.instance
+      .ref("Users")
+      .child(FirebaseAuth.instance.currentUser!.uid)
+      .child("School Visits");
 
-  Future<String> getData() async {
-    var uid = AuthService().getUser()!.uid;//Show only items who have the same uid
-    DatabaseReference reference = FirebaseDatabase.instance.ref("School Visits");
-    reference.onValue.listen((DatabaseEvent event) {
-      if (event.snapshot.exists) {
-        data = event.snapshot as List;
-        print(data);
-      } else {
-        print('No data available.');
-      }
-    });
-    print(data[1]["uid"]);
-
-    return "Success!";
-  }
   @override
-  void initState(){
-    this.getData();
+  void initState() {
+    super.initState();
+    _ref = FirebaseDatabase.instance
+        .ref("Users")
+        .child(FirebaseAuth.instance.currentUser!.uid)
+        .child("School Visits")
+        .orderByChild('school');
   }
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
-        backgroundColor: Colors.teal,
-        centerTitle: true,
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) =>Home()));
-            },
-            icon: Icon(
-              Icons.arrow_back_ios,
-              size: 20,
-              color: Colors.white,
-            )),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/stemrobo.png',
-              fit: BoxFit.contain,
-              height: 32,
-            ),
-          ],
-        ),
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: <Color>[Colors.tealAccent, Colors.teal]),
+
+  Widget _buildContactItem({required Map visitRecord}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.all(10),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Icon(
+                Icons.person,
+                color: Colors.black,
+                size: 20,
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                visitRecord['name'],
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              const Icon(
+                Icons.email,
+                color: Colors.black,
+                size: 14,
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                visitRecord['email'],
+                style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
           ),
-        ),
-      ),
-      body: new ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: 10,
-        // itemCount: data == null ? 0 : data.length,
-        itemBuilder: (BuildContext context, int index){
-          return makeCard;
-        },
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: [
+              const Icon(
+                Icons.school,
+                color: Colors.black,
+                size: 14,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Text(
+                visitRecord['school'],
+                style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Icon(
+                Icons.group_work,
+                color: Colors.black,
+                size: 14,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Text(
+                visitRecord['type'],
+                style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: [
+              const Icon(
+                Icons.notes,
+                color: Colors.black,
+                size: 14,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Text(
+                visitRecord['note'],
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: [
+              const Icon(
+                Icons.calendar_today,
+                color: Colors.black,
+                size: 14,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Text(
+                visitRecord['date'],
+                style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
-  final makeCard = Card(
-    elevation: 8.0,
-    margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-    child: Container(
-      decoration: BoxDecoration(color:Colors.teal.shade300),
-      child: makeListTile,
-    ),
-  );
 
-}
-final makeListTile = ListTile(
-    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-    leading: CircleAvatar(
-      child:  Icon(
-        Icons.add_to_photos_rounded,
-        size: 20,
-        color: Colors.black54,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('History'),
       ),
-      backgroundColor: Colors.white,
-    ),
-    title: Text(
-      "Name of Engineer ",
-      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-    ),
-    // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
-    subtitle: Row(
-      children: <Widget>[
-        Icon(Icons.linear_scale, color: Colors.yellowAccent),
-        Text(" Email", style: TextStyle(color: Colors.white))
-      ],
-    ),
-    trailing:
-    Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+      body: SizedBox(
+        height: double.infinity,
+        child: FirebaseAnimatedList(
+          query: _ref,
+          itemBuilder: (BuildContext context, DataSnapshot snapshot,
+              Animation<double> animation, int index) {
+            Map visitRecord = snapshot.value as Map;
+            visitRecord['key'] = snapshot.key;
+            return _buildContactItem(visitRecord: visitRecord);
+          },
+        ),
+      ),
+    );
+  }
+}
