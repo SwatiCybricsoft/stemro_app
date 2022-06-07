6ce475b49e3843354ffc44dee0a9c5248e515101
jugen <jugendrabhati658@gmail.com>
2022-06-07 15:24:24 +0530

    07-06-2022 || 15:00 PM

--- lib/form/old_visit_form.dart
+++ lib/form/old_visit_form.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import '../auth/home_page.dart';
 
 get isAdmin => isAdminStatus;
@@ -14,26 +14,22 @@ class OldVisits extends StatefulWidget {
 }
 
 class _OldVisits extends State<OldVisits> {
-
-  late Query _ref;
+  late Query _query;
 
   @override
   void initState() {
     super.initState();
-    if(isAdmin){
-      _ref = FirebaseDatabase.instance
-          .ref("School Visits")
-          .orderByChild('school');
-    }else {
-      _ref = FirebaseDatabase.instance
+    if (isAdmin) {
+      _query = FirebaseDatabase.instance.ref("School Visits");
+    } else {
+      _query = FirebaseDatabase.instance
           .ref("Users")
           .child(FirebaseAuth.instance.currentUser!.uid)
-          .child("School Visits")
-          .orderByChild('school');
+          .child("School Visits");
     }
   }
 
-  Widget _buildContactItem({required Map visitRecord}) {
+  Widget _buildRecordItem({required Map visitRecord}) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10),
       padding: const EdgeInsets.all(10),
@@ -163,6 +159,126 @@ class _OldVisits extends State<OldVisits> {
               ),
             ],
           ),
+          if (visitRecord.containsKey("images")) ...[
+            Row(
+              children: [
+                SizedBox(
+                  width: 370,
+                  height: 114,
+                  child: FirebaseAnimatedList(
+                    query: FirebaseDatabase.instance
+                        .ref("School Visits/" + visitRecord['key'] + "/images"),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                        Animation<double> animation, int index) {
+                      Map imageList = snapshot.value as Map;
+                      imageList['key'] = snapshot.key;
+                      return _buildImageItem(imageRecord: imageList);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+          if (visitRecord.containsKey("documents")) ...[
+            Row(
+              children: [
+                SizedBox(
+                  width: 370,
+                  height: 114,
+                  child: FirebaseAnimatedList(
+                    query: FirebaseDatabase.instance.ref(
+                        "School Visits/" + visitRecord['key'] + "/documents"),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                        Animation<double> animation, int index) {
+                      Map documentList = snapshot.value as Map;
+                      documentList['key'] = snapshot.key;
+                      return _buildDocumentItem(documentRecord: documentList);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImageItem({required Map imageRecord}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (imageRecord.containsKey("imageURL")) ...[
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () => {
+                    launch(imageRecord['imageURL'],
+                        forceSafariVC: true, forceWebView: true),
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Image.network(
+                        imageRecord['imageURL'],
+                        fit: BoxFit.cover,
+                        height: 90,
+                        width: 64,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDocumentItem({required Map documentRecord}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (documentRecord.containsKey("documentURL")) ...[
+            Row(
+              children: [
+                SizedBox(
+                  height: 80,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.green),
+                        padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(20)),
+                        textStyle: MaterialStateProperty.all(const TextStyle(
+                            fontSize: 14, color: Colors.white))),
+                    onPressed: () {
+                      var url = documentRecord['documentURL'];
+                      launch(url, forceSafariVC: true, forceWebView: true);
+                    },
+                    child: const Text(
+                      'View\nDocument',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ],
       ),
     );
@@ -177,12 +293,12 @@ class _OldVisits extends State<OldVisits> {
       body: SizedBox(
         height: double.infinity,
         child: FirebaseAnimatedList(
-          query: _ref,
+          query: _query.orderByChild('school'),
           itemBuilder: (BuildContext context, DataSnapshot snapshot,
               Animation<double> animation, int index) {
             Map visitRecord = snapshot.value as Map;
             visitRecord['key'] = snapshot.key;
-            return _buildContactItem(visitRecord: visitRecord);
+            return _buildRecordItem(visitRecord: visitRecord);
           },
         ),
       ),
